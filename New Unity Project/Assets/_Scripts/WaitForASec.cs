28feb03704f51f52abc627b2e3b86618a356374d
AustinMorrell <austinlm19@gmail.com>
2016-02-19 09:53:30 -0600

    Poke'Touhou
    Gave the game a name

---
+++ New Unity Project/Assets/_Scripts/WaitForASec.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WaitForASec : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject NewGame;
+    [SerializeField]
+    private GameObject Continue;
+    [SerializeField]
+    private GameObject Exit;
+    private float count;
+    private bool check;
+
+	void Start ()
+    {
+        NewGame.SetActive(false);
+        Continue.SetActive(false);
+        Exit.SetActive(false);
+        check = true;
+	}
+	
+
+	void Update ()
+    {
+        count += Time.deltaTime;
+        if (check)
+        {
+            if (count >= 2)
+            {
+                NewGame.SetActive(true);
+                Continue.SetActive(true);
+                Exit.SetActive(true);
+                check = false;
+            }
+        }
+    }
+}
