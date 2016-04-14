2057073ea7953b15ed67854926cdba75da42b32c
Austin Morrell <austinlm19@gmail.com>
2016-04-14 13:02:47 -0500

    Cool new Mega evoled form

--- Poke'Touhou/Assets/_Scripts/Player.cs
+++ Poke'Touhou/Assets/_Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour {
 
     [SerializeField]
     private float m_speed;
+    [SerializeField]
+    private Sprite m_Mega;
     [ReadOnly, SerializeField]
     private float m_BaseHp;
     [ReadOnly, SerializeField]
@@ -13,9 +15,12 @@ public class Player : MonoBehaviour {
     private float m_PlayerHp;
     [ReadOnly, SerializeField]
     private float m_PlayerPower;
+    [ReadOnly, SerializeField]
+    private int m_Mode;
     [SerializeField]
     protected GameObject Fire;
     protected GameObject TempFire;
+    public int Mode { get { return m_Mode; } set { m_Mode = value; } }
     public float power { get { return m_PlayerPower; } set { m_PlayerPower = value; } }
     public float speed { get { return m_speed; } set { m_speed = value; } }
     public float BasePower { get { return m_BasePower; } set { m_BasePower = value; } }
@@ -35,6 +40,7 @@ public class Player : MonoBehaviour {
 
     void Awake()
     {
+        m_Mode = 1;
         gameObject.transform.position = new Vector3(0, -4, 0);
         m_BaseHp = 40;
         m_PlayerHp = m_BaseHp;
@@ -57,17 +63,47 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
+        if (m_PlayerPower > 999)
+        {
+            m_Mode = 2;
+            gameObject.GetComponent<SpriteRenderer>().sprite = m_Mega;
+        }
+
         Lerp(Input.GetAxis("LeftJoystickX") * speed, -Input.GetAxis("LeftJoystickY") * speed, 0);
 
     if (Input.GetButton("A"))
         {
-            if (DoIt)
+            if (Mode == 1)
             {
-                TempFire = Instantiate(Fire, gameObject.transform.position, Quaternion.identity) as GameObject;
-                TempFire.GetComponent<Rigidbody>().velocity = new Vector3(0, 20, 0);
-                TempFire.transform.Rotate(new Vector3(0, 0, 90));
-                DoIt = false;
+                if (DoIt)
+                {
+                    TempFire = Instantiate(Fire, gameObject.transform.position, Quaternion.identity) as GameObject;
+                    TempFire.GetComponent<Rigidbody>().velocity = new Vector3(0, 20, 0);
+                    TempFire.transform.Rotate(new Vector3(0, 0, 90));
+                    DoIt = false;
+                }
             }
+
+            if (Mode == 2)
+            {
+                if (DoIt)
+                {
+                    TempFire = Instantiate(Fire, gameObject.transform.position, Quaternion.identity) as GameObject;
+                    TempFire.GetComponent<Rigidbody>().velocity = new Vector3(0, 20, 0);
+                    TempFire.transform.Rotate(new Vector3(0, 0, 90));
+
+                    TempFire = Instantiate(Fire, gameObject.transform.position, Quaternion.identity) as GameObject;
+                    TempFire.GetComponent<Rigidbody>().velocity = new Vector3(20, 20, 0);
+                    TempFire.transform.Rotate(new Vector3(0, 0, 45));
+
+                    TempFire = Instantiate(Fire, gameObject.transform.position, Quaternion.identity) as GameObject;
+                    TempFire.GetComponent<Rigidbody>().velocity = new Vector3(-20, 20, 0);
+                    TempFire.transform.Rotate(new Vector3(0, 0, 135));
+
+                    DoIt = false;
+                }
+            }
+
             if (DoIt == false)
             {
                 Counter += Time.deltaTime;
