c60466517de42e32529ed64177b84d502ff3bec1
Austin Morrell <austinlm19@gmail.com>
2016-05-04 13:45:35 -0500

    Alot of changes

---
+++ Poke'Touhou/Assets/_Scripts/CelebiAttck.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CelebiAttck : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject Fire;
+    [SerializeField]
+    protected Player Player;
+    [SerializeField]
+    protected float WaitForFire;
+    protected float count;
+    protected bool DoIt;
+    [SerializeField]
+    protected float Speed;
+    public bool m_Attack = true;
+    protected GameObject TempFireball;
+    [SerializeField]
+    protected int Amount;
+    protected float OffSet;
+
+    protected void Start()
+    {
+        DoIt = false;
+    }
+
+    protected void Update()
+    {
+        if (m_Attack)
+        {
+            if (DoIt && (Player.PlayerHp > 0))
+            {
+                for (int i = 0; i < Amount; i++)
+                {
+                    TempFireball = Instantiate(Fire, gameObject.transform.position, Quaternion.identity) as GameObject;
+                    float ATang = Mathf.Atan((transform.position.y - Player.transform.position.y) / (transform.position.x - Player.transform.position.x));
+
+                    // Correct the angle if we are in quadrant 1
+                    if (transform.position.x > Player.transform.position.x && transform.position.y > Player.transform.position.y)
+                        ATang += Mathf.PI;
+                    // Correct the angle if we are in quadrant 4
+                    if (transform.position.x > Player.transform.position.x && transform.position.y < Player.transform.position.y)
+                        ATang -= Mathf.PI;
+
+                    ATang += OffSet;
+                    OffSet += Amount * (Mathf.PI / 180);
+                    TempFireball.GetComponent<Rigidbody>().velocity = new Vector3(Speed * Mathf.Cos(ATang), Speed * Mathf.Sin(ATang), 0);
+                    TempFireball.transform.Rotate(new Vector3(0, 0, ATang * (180 / Mathf.PI)));
+                }
+                DoIt = false;
+            }
+            else
+            {
+                count += Time.deltaTime;
+                if (count >= WaitForFire)
+                {
+                    DoIt = true;
+                    count = 0;
+                }
+            }
+        }
+    }
+}
--- Poke'Touhou/Assets/_Scripts/WaitForASec.cs
+++ Poke'Touhou/Assets/_Scripts/WaitForASec.cs
@@ -10,15 +10,28 @@ public class WaitForASec : MonoBehaviour {
     private GameObject Continue;
     [SerializeField]
     private GameObject Exit;
+    [SerializeField]
+    private GameObject Handle;
+    [SerializeField]
+    private GameObject N;
+    [SerializeField]
+    private GameObject C;
+    [SerializeField]
+    private GameObject E;
+    private int Selected;
     private float count;
     private bool check;
+    private bool Hold;
 
 	void Start ()
     {
         NewGame.SetActive(false);
         Continue.SetActive(false);
         Exit.SetActive(false);
+        Handle.SetActive(false);
+        Hold = true;
         check = true;
+        Selected = 1;
 	}
 	
 
@@ -32,7 +45,51 @@ public class WaitForASec : MonoBehaviour {
                 NewGame.SetActive(true);
                 Continue.SetActive(true);
                 Exit.SetActive(true);
+                Handle.SetActive(true);
                 check = false;
+            }
+        }
+
+        if (Input.GetAxis("LeftJoystickY") > 0 && Hold)
+        {
+            Selected++;
+            if (Selected > 3) { Selected = 1; }
+            switch (Selected)
+            {
+                case 1:
+                    Handle.transform.position = new Vector3(Handle.transform.position.x, -0.5f, Handle.transform.position.z);
+                    break;
+
+                case 2:
+                    Handle.transform.position = new Vector3(Handle.transform.position.x, -1.5f, Handle.transform.position.z);
+                    break;
+
+                case 3:
+                    Handle.transform.position = new Vector3(Handle.transform.position.x, -2.5f, Handle.transform.position.z);
+                    break;
+            }
+            Hold = false;
+        }
+
+        if (Input.GetAxis("LeftJoystickY") == 0)
+        {
+            Hold = true;
+        }
+
+        if (Input.GetButtonDown("A"))
+        {
+            switch (Selected)
+            {
+                case 1:
+                    N.GetComponent<StartGame>().OnClick();
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    E.GetComponent<Exit>().OnClick();
+                    break;
             }
         }
     }
